abecb214de5b6fb87d5481e12177c2103d0a10dd
xpandaao <ida.marie132@gmail.com>
2023-03-29 17:27:59 +0200

    Small changes

--- ms/src/components/moviecards.js
+++ ms/src/components/moviecards.js
@@ -1,10 +1,10 @@
 import MovieCard from "./moviecard";
-import Search from './search'
+import SearchBar from './search'
 
 export default function MovieCards({movies, setSearchBar}){
     return(
         <>
-        <Search setSearchBar={setSearchBar} />
+        <SearchBar setSearchBar={setSearchBar} />
         <section className="movies-list">
             {movies.map((movie, index) => ( <MovieCard key={index} img={movie.Poster} title={movie.Title} year={movie.Year}
             gen={movie.Genre} dir={movie.Director} act={movie.Actors} imdbRating={movie.Rating} awards={movie.Awards} slug={movie?.movie?.label.replace(/\s/g, "-").toLowerCase()}/>))}
